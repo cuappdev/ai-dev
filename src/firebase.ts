48a53f7dc2786179d0043f2711ed7cff3f36152f
Aayush Agnihotri <aa2328@cornell.edu>
2024-12-03 17:36:49 -0500

    Fixing firebase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -6,10 +6,12 @@ const firebaseConfig = {
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID!,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID!
 };
+
+console.log("Config:", firebaseConfig);
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
