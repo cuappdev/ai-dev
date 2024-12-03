947a4293b95b6c3b9c7eb3096d5a81dc120d2781
Aayush Agnihotri <aa2328@cornell.edu>
2024-12-03 18:38:06 -0500

    firebase

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,25 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY?.replace(/^"|"$/g, '') || "";
-const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN?.replace(/^"|"$/g, '') || "";
-const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID?.replace(/^"|"$/g, '') || "";
-const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET?.replace(/^"|"$/g, '') || "";
-const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID?.replace(/^"|"$/g, '') || "";
-const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID?.replace(/^"|"$/g, '') || "";
-const measurementId = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID?.replace(/^"|"$/g, '') || "";
-
+// Doesn't need to be private
 const firebaseConfig = {
-  apiKey: apiKey,
-  authDomain: authDomain,
-  projectId: projectId,
-  storageBucket: storageBucket,
-  messagingSenderId: messagingSenderId,
-  appId: appId,
-  measurementId: measurementId
+  apiKey: "AIzaSyCNf8_FlIH1G7f3lH5ZBQwIKkwi4M5vib4",
+  authDomain: "ai-dev-24263.firebaseapp.com",
+  projectId: "ai-dev-24263",
+  storageBucket: "",
+  messagingSenderId: "591469176148",
+  appId: "1:591469176148:web:f93ba3a2b41fe4bf4cd4c1",
+  measurementId: "G-3D6EFZBVM1"
 };
-
-console.log("Config:", firebaseConfig);
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app)
