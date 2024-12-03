a785995aca1ec403e8deaa1ff31dce4224087a24
Aayush Agnihotri <aa2328@cornell.edu>
2024-12-03 17:59:43 -0500

    replace

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,15 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// .replace(/^"|"$/g, '')
-
-const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
-const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
-const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
-const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID;
-const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
-const measurementId = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
+const apiKey = process.env.NEXT_PUBLIC_FIREBASE_API_KEY?.replace(/^"|"$/g, '') || "";
+const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN?.replace(/^"|"$/g, '') || "";
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID?.replace(/^"|"$/g, '') || "";
+const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET?.replace(/^"|"$/g, '') || "";
+const messagingSenderId = process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID?.replace(/^"|"$/g, '') || "";
+const appId = process.env.NEXT_PUBLIC_FIREBASE_APP_ID?.replace(/^"|"$/g, '') || "";
+const measurementId = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID?.replace(/^"|"$/g, '') || "";
 
 const firebaseConfig = {
   apiKey: apiKey,
