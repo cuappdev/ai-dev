ffb4ec4b364f198646dfcb057101f5c66688c9aa
Aayush Agnihotri <aa2328@cornell.edu>
2024-12-20 20:00:30 -0500

    removing requires

--- src/app/api/firebase-admin-config.ts
+++ src/app/api/firebase-admin-config.ts
@@ -1,10 +1,8 @@
-const admin = require("firebase-admin");
-
-const serviceAccount = require("../../../firebase-service-account.json");
+import * as admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert("../../../firebase-service-account.json")
   });
 }
 
