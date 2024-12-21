62021cbf0db4ee188f080951c646de8c07784003
Aayush Agnihotri <aa2328@cornell.edu>
2024-12-21 11:25:54 -0500

    Adding logs

--- src/firebase-admin-config.ts
+++ src/firebase-admin-config.ts
@@ -3,8 +3,17 @@ import * as admin from "firebase-admin";
 let adminAuth: admin.auth.Auth | null = null;
 
 const getServiceAccount = () => {
-  console.log(process.env.ADMIN_FIREBASE_PRIVATE_KEY);
+  console.log("From env", process.env.ADMIN_FIREBASE_PRIVATE_KEY);
+  console.log("\nParsed", JSON.parse(process.env.ADMIN_FIREBASE_PRIVATE_KEY!));
   const { privateKey } = JSON.parse(process.env.ADMIN_FIREBASE_PRIVATE_KEY!);
+  console.log("\nPrivate key", privateKey);
+  console.log("\nProject ID", process.env.ADMIN_FIREBASE_PROJECT_ID);
+  console.log("\nClient email", process.env.ADMIN_FIREBASE_CLIENT_EMAIL);
+  console.log("\nService account", {
+    projectId: process.env.ADMIN_FIREBASE_PROJECT_ID,
+    clientEmail: process.env.ADMIN_FIREBASE_CLIENT_EMAIL,
+    privateKey,
+  });
 
   return {
     projectId: process.env.ADMIN_FIREBASE_PROJECT_ID,
