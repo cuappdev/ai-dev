3493866604c54e0c47b3fb6c4e2ab5f0654ad431
Aayush Agnihotri <aa2328@cornell.edu>
2024-12-22 14:02:44 -0500

    removing unused code

--- src/app/utils/requestUtils.ts
+++ src/app/utils/requestUtils.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { ApiError } from '@/types/ApiError';
 
 export async function createClonedRequest(request: NextRequest) {
   const init: RequestInit = {
@@ -57,10 +56,9 @@ export async function cloneRequest(request: NextRequest, url: string) {
   }
 }
 
-// export async function isUserInDatabase(firebaseUser: any) {
-//   console.log(firebaseUser);
-//   // TODO: Check if user is in database
-//   // const uid = firebaseUser.uid;
-//   // return NextResponse.json('You must be part of Cornell AppDev to use this app', { status: 403 });
-//   return true;
-// }
+export async function getUserFromDatabase(uid: string) {
+  console.log(uid);
+  // TODO: Check if user is in database
+  // Return user if it exists, otherwise return null
+  return true;
+}
