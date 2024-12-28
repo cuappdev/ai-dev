a4177b9ff7936ca0e513e2194e199e0d8b856ef0
Aayush Agnihotri <aa2328@cornell.edu>
2024-12-27 21:46:37 -0500

    Changing db utils

--- src/app/utils/databaseUtils.ts
+++ src/app/utils/databaseUtils.ts
@@ -1,4 +1,5 @@
 import prisma from '@/prisma';
+import { User } from '@prisma/client';
 
 export async function createEmail(email: string) {
   return await prisma.email.create({
@@ -24,22 +25,7 @@ export async function deleteEmail(email: string) {
   });
 }
 
-export async function upsertUserFromEmail(email: { id: string; email: string }, uid: string) {
-  return await prisma.user.upsert({
-    where: {
-      uid: uid,
-    },
-    create: {
-      uid: uid,
-      emailId: email.id,
-    },
-    update: {
-      emailId: email.id,
-    },
-  });
-}
-
-export async function getUser(uid: string) {
+export async function getUser(uid: string): Promise<User | null> {
   return await prisma.user.findUnique({
     where: {
       uid: uid,
